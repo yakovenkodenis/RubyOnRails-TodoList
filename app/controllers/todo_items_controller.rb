8101da6588fb351be01206aa20922fa7b2998159
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-03-12 22:56:35 +0200

    Added delete specs for todo list items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,15 +1,15 @@
 class TodoItemsController < ApplicationController
+
+	before_action :find_todo_list
+
 	def index
-		@todo_list = TodoList.find(params[:todo_list_id])
 	end
 
 	def new
-		@todo_list = TodoList.find(params[:todo_list_id])
 		@todo_item = @todo_list.todo_items.new
 	end
 
 	def create
-		@todo_list = TodoList.find(params[:todo_list_id])
 		@todo_item = @todo_list.todo_items.new(todo_item_params)
 		if @todo_item.save
 			flash[:success] = "Added todo list item."
@@ -21,7 +21,6 @@ class TodoItemsController < ApplicationController
 	end
 
 	def update
-		@todo_list = TodoList.find(params[:todo_list_id])
 		@todo_item = @todo_list.todo_items.find(params[:id])
 		if @todo_item.update_attributes(todo_item_params)
 			flash[:success] = "Saved todo list item."
@@ -34,8 +33,17 @@ class TodoItemsController < ApplicationController
 
 
 	def edit
-		@todo_list = TodoList.find(params[:todo_list_id])
 		@todo_item = @todo_list.todo_items.find(params[:id])
+	end
+
+	def destroy
+		@todo_item = @todo_list.todo_items.find(params[:id])
+		if @todo_item.destroy
+			flash[:success] = "Todo list item was deleted."
+		else
+			flash[:error] = "Todo list item could not be deleted."
+		end
+		redirect_to todo_list_todo_items_path
 	end
 
 	def url_options
@@ -44,6 +52,10 @@ class TodoItemsController < ApplicationController
 
 
 	private
+
+	def find_todo_list
+		@todo_list = TodoList.find(params[:todo_list_id])
+	end
 
 	def todo_item_params
 		params[:todo_item].permit(:content)
